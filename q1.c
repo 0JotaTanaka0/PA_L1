935a23e853eef67a537d8ef26b4fe4fa21ff32d4
0JotaTanaka0 <TanakaCoder370@gmail.com>
2024-03-25 02:04:03 -0300

    commit 1

---
+++ q1.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int i=3, j=5;
+    int *p, *q;
+    p = &i;
+    q = &j;
+
+    int w =  p == &i;
+    int x =  *p - *q;
+    int y =  **&p;
+    int z =  3 - *p/(*q) + 7;
+
+    printf("O valor de p==&i = %d\n", w);
+    printf("O valor de *p - *q = %d\n", x);
+    printf("O valor de **&p = %d\n", y);
+    printf("O valor de 3 - *p/(*q) + 7 = %d\n", z);
+
+    return 0;
+}
